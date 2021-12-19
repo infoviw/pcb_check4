5d1226f35f755699efa3b8dca3368e8fa2eb36a5
infoviw <viwicodes@gmail.com>
2021-12-19 10:48:22 +0530

    cc

---
+++ sm/script.js
@@ -0,0 +1,29 @@
+new Vue({
+  el: '#app',
+  data: () => {
+    return {
+      icons: [
+        {
+          socialIcon: 'fa fa-facebook',
+          socialLink: ''
+        },
+        {
+          socialIcon: 'fa fa-google',
+          socialLink: ''
+        },
+        {
+          socialIcon: 'fa fa-github',
+          socialLink: ''
+        },
+        {
+          socialIcon: 'fa fa-youtube',
+          socialLink: ''
+        },
+        {
+          socialIcon: 'fa fa-codepen',
+          socialLink: ''
+        }
+      ]
+    }
+  }
+})
